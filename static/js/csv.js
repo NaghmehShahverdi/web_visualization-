4531f730e5703125078caac3e52500ef5754ccff
Mshzsh <mshzsh@gmail.com>
2023-09-11 10:56:21 -0700

    Fixing a issue in the csv downloaded file name

--- static/js/csv.js
+++ static/js/csv.js
@@ -1,69 +1,76 @@
-const downloadButtonContainer = document.getElementById("download_button_container");
+const downloadButtonContainer = document.getElementById(
+  "download_button_container"
+);
+const urlParams = new URLSearchParams(window.location.search);
+
 let data = null;
 let columns = null;
 
-function setDownloadInfo (gridColumns, gridData) {
+function setDownloadInfo(gridColumns, gridData) {
   columns = gridColumns;
   data = gridData;
 }
 
 function renderDownloadButton() {
-  const download_button = document.createElement('button');
-  download_button.id = "download_button"
-  download_button.textContent = 'Download as CSV';
-  download_button.addEventListener('click', () => {downloadCSV()});
+  const download_button = document.createElement("button");
+  download_button.id = "download_button";
+  download_button.textContent = "Download as CSV";
+  download_button.addEventListener("click", () => {
+    downloadCSV();
+  });
   downloadButtonContainer.appendChild(download_button);
 }
 
-function makeCSV () {
+function makeCSV() {
   if (data !== null) {
-    var csv_rows = [columns.join(",")]
-    for (row of data){
-      csv_rows.push (row.join(","))
+    var csv_rows = [columns.join(",")];
+    for (row of data) {
+      csv_rows.push(row.join(","));
     }
-    csv = csv_rows.join ("\n")
-    console.log (csv)
-    return csv
+    csv = csv_rows.join("\n");
+    console.log(csv);
+    return csv;
   }
-  const table_html = document.getElementById("results")
-  console.log (table_html.getElementsByClassName("gridjs-tr"))
+  const table_html = document.getElementById("results");
+  console.log(table_html.getElementsByClassName("gridjs-tr"));
 }
 
 function downloadCSV() {
   // Make the CSV
-  const csv_data = makeCSV ();
+  const csv_data = makeCSV();
   let title = "Title";
 
-  const log_p_val = document.getElementById("textInput1").value; // -log(P-Value) Threshold
-  const spe_rank = document.getElementById("textInput2").value; // Specificity Rank
-  const cluster = document.getElementById("textInput3").value; // Cluster ID
-  const gene = document.getElementById("textInput4").value; // Gene
+  const _log_p_val = urlParams.get("scz_2022_p"); // -log(P-Value) Threshold
+  const _spe_rank = urlParams.get("spe_rank"); // Specificity Rank
+  const _cluster = urlParams.get("cluster"); // Cluster ID
+  const _gene = urlParams.get("gene"); // Gene
+  const _visualization = urlParams.get("visualization"); // Gene
 
-  if (visualization.value === "option1") {
-    title = `Cluster-Specific Based on -log(p-value) threshold ${log_p_val}`
+  if (_visualization === "option1") {
+    title = `Cluster-Specific Based on -log(p-value) threshold ${_log_p_val}`;
   }
-  if (visualization.value === "option2") {
-    title = `Gene Specificity in Cluster ${cluster} (with -log(P-Value) threshold ${log_p_val})`
+  if (_visualization === "option2") {
+    title = `Gene Specificity in Cluster ${_cluster} (with -log(P-Value) threshold ${_log_p_val})`;
   }
-  if (visualization.value === "option3") {
-    title = `Specificity Profile of Gene ${gene} Across Clusters`
+  if (_visualization === "option3") {
+    title = `Specificity Profile of Gene ${_gene} Across Clusters`;
   }
-  if (visualization.value === "option4") {
-    title = `Gene P-value in Cluster ${cluster} (with specificity rank threshold ${spe_rank})`
+  if (_visualization === "option4") {
+    title = `Gene P-value in Cluster ${_cluster} (with specificity rank threshold ${_spe_rank})`;
   }
-  if (visualization.value === "option5") {
-    title = `Cluster ${cluster} Information`
+  if (_visualization === "option5") {
+    title = `Cluster ${_cluster} Information`;
   }
 
-  console.log (title)
+  console.log(title);
 
   // Create CSV file object and feed our csv_data into it
   const CSVFile = new Blob([csv_data], {
-    type: "text/csv"
+    type: "text/csv",
   });
 
   // Create to temporary link to initiate download process
-  var temp_link = document.createElement('a');
+  var temp_link = document.createElement("a");
   // Download csv file
   temp_link.download = title;
   var url = window.URL.createObjectURL(CSVFile);
@@ -78,7 +85,7 @@ function downloadCSV() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  if (visualization.value !== "option0"){
-    renderDownloadButton ();
+  if (visualization.value !== "option0") {
+    renderDownloadButton();
   }
-})
+});
